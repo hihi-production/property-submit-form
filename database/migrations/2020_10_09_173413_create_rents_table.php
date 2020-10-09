61e00365087be68106fe9906f0a8cfe1e681913c
Akhmad Rizki Prayoga <akhmadrizki24@gmail.com>
2020-10-10 02:10:51 +0800

    create model and migration

---
+++ database/migrations/2020_10_09_173413_create_rents_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rents', function (Blueprint $table) {
+            $table->id();
+            $table->string('rented_name');
+            $table->string('owner_name');
+            $table->string('owner_phone');
+            $table->string('rented_address');
+            $table->string('type');
+            $table->integer('price');
+            $table->string('additional');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rents');
+    }
+}
---
+++ resources/views/layouts/landing.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Kontrak.in</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
+<body>
+    <div class="wrapper">
+        @include('client.partials.header')
+
+            @yield('content')
+
+        @include('client.partials.footer')
+    </div>
+</body>
+</html>
