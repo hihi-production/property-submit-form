50f6fcf1fdad57b8e01150a292b701b0bacde7b7
YossiDarmawan <yossidarmawan@gmail.com>
2020-10-11 13:26:08 +0800

    fix register

--- resources/views/dashboard/register.blade.php
+++ resources/views/dashboard/register.blade.php
@@ -33,7 +33,7 @@
         <div class="row">
           <div class="col-12 col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-8 offset-lg-2 col-xl-8 offset-xl-2">
             <div class="login-brand">
-                <p class="text-center"> <img src="{{asset('img/primary-logo.png')}}" alt="logo" width="250" class="shadow-light" ></p>
+                <p class="text-center"> <img src="{{asset('img/primary-logo.png')}}" alt="logo" width="250" ></p>
             </div>
 
             <div class="card card-primary">
